d49b1da6aa429e289edf12461f32eb6e6a2e0a0c
Montassare Zekri <montassare6@gmail.com>
2023-06-29 13:27:24 +0100

    added limit and offset to getData

--- lib/controllers/db_controller.dart
+++ lib/controllers/db_controller.dart
@@ -1,6 +1,6 @@
-import 'package:mFirebase/utilities/server_response.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:mFirebase/utilities/server_response.dart';
 
 class DatabaseService {
   final FirebaseDatabase _db = FirebaseDatabase.instance;
@@ -8,31 +8,47 @@ class DatabaseService {
     _db.databaseURL = databaseUrl;
     print("✅ Database URL setted : ${_db.databaseURL}");
   }
-
-  Future<ServerResponse> getData({required String nodePath}) async {
-    print("🚀 Getting data from $nodePath ...");
+  Future<ServerResponse> getData({
+    required String nodePath,
+    int limit = 10,
+    int offset = 0,
+  }) async {
+    print(
+        "🚀 Getting data from $nodePath with limit $limit and offset $offset ...");
     try {
-      DatabaseEvent data = await _db.ref(nodePath).once();
+      final DatabaseEvent data =
+          await _db.ref(nodePath).limitToFirst(limit).startAt(offset).once();
       if (data.snapshot.value != null) {
-        return ServerResponse<dynamic>(ResponseStatus.Success,
-            data: data.snapshot.value);
+        return ServerResponse<dynamic>(
+          ResponseStatus.Success,
+          data: data.snapshot.value,
+        );
       } else {
-        return ServerResponse(ResponseStatus.Error,
-            errorMessage: "Unknown Error");
+        return ServerResponse(
+          ResponseStatus.Error,
+          errorMessage: "Unknown Error",
+        );
       }
     } on FirebaseException catch (e, stk) {
       print("🚨 Firebase exception $e");
       print("🚨 Firebase exception stacktrace $stk");
-      return ServerResponse(ResponseStatus.Error, errorMessage: e.message);
+      return ServerResponse(
+        ResponseStatus.Error,
+        errorMessage: e.message,
+      );
     } catch (e, stk) {
       print("🚨 Exception $e");
       print("🚨 Exception stacktrace $stk");
-      return ServerResponse(ResponseStatus.Error, errorMessage: e.toString());
+      return ServerResponse(
+        ResponseStatus.Error,
+        errorMessage: e.toString(),
+      );
     }
   }
 
   Stream<DatabaseEvent> getDataAsStream({required String nodePath}) {
-    DatabaseReference streamData = FirebaseDatabase.instance.ref(nodePath);
+    final DatabaseReference streamData =
+        FirebaseDatabase.instance.ref(nodePath);
     return streamData.onValue;
   }
 }
